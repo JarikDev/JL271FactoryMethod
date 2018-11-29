20d6fba4283951031857e48b949289f01782c719
ivan4000 <sxln1@mail.ru>
2018-11-29 22:13:31 +0300

    JL271 factory method

---
+++ src/com/Main.java
@@ -0,0 +1,14 @@
+package com;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person=Person.create();
+    }
+}
+class Person{
+    private Person() {
+    }
+    public static Person create(){
+        return new Person();
+    }
+}
